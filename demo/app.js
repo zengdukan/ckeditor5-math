0bee5eec7ab07b4de1f370724595ea8485f92d9e
zengdukan <zengdukan@163.com>
2022-05-10 17:42:58 +0800

    add plugin

--- demo/app.js
+++ demo/app.js
@@ -4,23 +4,22 @@ import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
-
 import Math from "../src/math";
 import AutoformatMath from "../src/autoformatmath";
 
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
-
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 
 ClassicEditor.create(document.querySelector("#editor"), {
-	plugins: [Essentials, Paragraph, Bold, Italic, Math, AutoformatMath, SourceEditing],
+	plugins: [Essentials, Paragraph, Bold, Italic, Math, AutoformatMath, SourceEditing,  Autoformat],
 	toolbar: ["bold", "italic", "math",'sourceEditing'],
 		enablePreview: true, // Enable preview view
 		math: { engine: "katex", outputType: 'span' },
 })
 	.then((editor) => {
 		console.log("Editor was initialized", editor);
-		CKEditorInspector.attach(editor)
+		CKEditorInspector.attach(editor);
 	})
 	.catch((error) => {
 		console.error(error);
